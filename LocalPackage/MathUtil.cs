eb03843edf6241f5773d0ca859f1c8943a8ba710
netpyoung <netpyoung@gmail.com>
2024-10-30 07:48:44 +0900

    doc

--- LocalPackage/MathUtil.cs
+++ LocalPackage/MathUtil.cs
@@ -6,11 +6,13 @@ namespace NF.UnityLibs.Utils
     public static class MathUtil
     {
         /// <summary>
-        /// .NET의 Math.Round 메서드는 기본적으로 "Banker's Rounding" 방식을 사용. 가장 가까운 짝수로 반올림.
+        /// Round_Normal 은 일반적인 반올림 (Round Half Up).
+        ///
+        /// .NET의 Math.Round 메서드는 기본적으로 "Banker's Rounding" 방식을 사용. 가장 가까운 짝수로 반올림. 실수하기 쉬움.
         /// 
         /// ```cs
-        /// Math.Round(2.5); // 결과: 2
-        /// Math.Round(2.5, MidpointRounding.AwayFromZero); // 결과: 3
+        /// Math.Round(2.5, MidpointRounding.AwayFromZero); // 결과: 3 | Round Half Up
+        /// Math.Round(2.5);                                // 결과: 2 | Round Half to Even
         /// ```
         /// </summary>
         /// <param name="v"></param>
