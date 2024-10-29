9c70a1ea2d5644ecf0e0396d2f8ad804e0d3f40a
netpyoung <netpyoung@gmail.com>
2024-10-29 15:43:58 +0900

    add: MathRoundCodeFixProvider.cs

---
+++ RoslynCodeAnalysis/NF.UnityLibs.Utils.RoslynCodeAnalysis/DiagnosticAnalyzers/MathRoundAnalyzer .cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NF.UnityLibs.Utils.RoslynCodeAnalysis.DiagnosticDescriptors;
+using System.Collections.Immutable;
+
+namespace NF.UnityLibs.Utils.RoslynCodeAnalysis.DiagnosticAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MathRoundAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(DiagnosticDescriptorCollection.NF5001);
+            }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            InvocationExpressionSyntax invocation = (InvocationExpressionSyntax)context.Node;
+
+            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
+            {
+                if (memberAccess.Name.Identifier.Text == "Round"
+                    && memberAccess.Expression is IdentifierNameSyntax identifierName
+                    && identifierName.Identifier.Text == "Math")
+                {
+                    Diagnostic diagnostic = Diagnostic.Create(DiagnosticDescriptorCollection.NF5001, invocation.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
---
+++ RoslynCodeAnalysis/NF.UnityLibs.Utils.RoslynCodeAnalysis/DiagnosticDescriptors/DiagnosticDescriptorCollection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace NF.UnityLibs.Utils.RoslynCodeAnalysis.DiagnosticDescriptors
+{
+    internal class DiagnosticDescriptorCollection
+    {
+        internal static readonly DiagnosticDescriptor NF5001 = new(
+            id: "NF5001", //
+            title: "Use MathUtil.Round_Normal instead of Math.Round",
+            messageFormat: "Use MathUtil.Round_Normal instead of Math.Round for normal rounding. {0}", //
+            category: "Usage",
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+            //description: "Math.Round uses banker's rounding by default. Use MathUtil.Round_Normal for normal rounding."
+        );
+    }
+}
